9520198ee913c1b96131f5fad86d55d106c7fa64
Jetic Gu <jetic@me.com>
2018-10-12 12:32:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="natlang",
-    version="0.3.a2",
+    version="0.3.a3",
     author="Jetic Gū, Rory Wang",
     author_email="jeticg@sfu.ca",
     description="Natural language data loading tools",
