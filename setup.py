63bcf7c8d1147e2584e6a8dd69beda35de2704be
Jetic Gu <jetic@me.com>
2018-10-15 08:44:22 -0700

    pyCodeSyntax checker (0.3a6)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="natlang",
-    version="0.3.a5",
+    version="0.3.a6",
     author="Jetic Gū, Rory Wang",
     author_email="jeticg@sfu.ca",
     description="Natural language data loading tools",
