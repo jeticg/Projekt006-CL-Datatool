797bc70bd9cc91685756293976f723704d56dd3f
Jetic Gū <jeticg@sfu.ca>
2018-12-21 18:41:28 +0800

    Bidirectional unbatched tagger trainer

--- natlang/model/labeller/BiLSTM.py
+++ natlang/model/labeller/BiLSTM.py
@@ -10,7 +10,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import tqdm
 
+import natlang as nl
 torch.manual_seed(1)
 
 
@@ -24,45 +26,61 @@ def convert2Index(seq, to_ix):
     return torch.tensor(idxs, dtype=torch.long)
 
 
-training_data = [
-    ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
-    ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
-]
-word_to_ix = {}
-for sent, tags in training_data:
-    for word in sent:
-        if word not in word_to_ix:
-            word_to_ix[word] = len(word_to_ix)
-print(word_to_ix)
-tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
-ix_to_tag = ["DET", "NN", "V"]
+def extractData(files):
+    def separateTag(sequence):
+        return ([entry[1] for entry in sequence],
+                [entry[0] for entry in sequence])
+    loader = nl.loader.DataLoader("tree")
+    data = loader.load(files)
+    data = [separateTag(entry.phrase) for entry in data]
+    return data
+
+
+trainData = extractData("~/Daten/syntactic-data/Penn_Treebank/train.tree.en")
+valData = extractData("~/Daten/syntactic-data/Penn_Treebank/test1.tree.en")
+testData = extractData("~/Daten/syntactic-data/Penn_Treebank/test2.tree.en")
+
+w2int = {}
+t2int = {}
+int2t = []
+for sentence, tag in trainData:
+    for w in sentence:
+        if w not in w2int:
+            w2int[w] = len(w2int)
+    for t in tag:
+        if t not in t2int:
+            t2int[t] = len(t2int)
+            int2t.append(t)
 
 # These will usually be more like 32 or 64 dimensional.
 # We will keep them small, so we can see how the weights change as we train.
-EMBEDDING_DIM = 6
-HIDDEN_DIM = 6
+EMBEDDING_DIM = 300
+HIDDEN_DIM = 300
 
 
 class Tagger(nn.Module):
-    def __init__(self, inDim, hidDim, vocab_size, tagset_size):
+    def __init__(self, inDim, hidDim, layers, vocab_size, tagset_size):
         super(Tagger, self).__init__()
         self.hidDim = hidDim
+        self.layers = layers
 
         self.word_embeddings = nn.Embedding(vocab_size, inDim)
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidDim.
-        self.lstm = nn.LSTM(inDim, hidDim)
+        self.lstm = nn.LSTM(inDim, hidDim,
+                            num_layers=self.layers,
+                            bidirectional=True)
 
         # The linear layer that maps from hidden state space to tag space
-        self.hidden2tag = nn.Linear(hidDim, tagset_size)
+        self.hidden2tag = nn.Linear(hidDim * self.layers, tagset_size)
         self.hidden = self.init_hidden()
         self.lossFunc = nn.NLLLoss()
         return
 
     def init_hidden(self):
-        return (torch.zeros(1, 1, self.hidDim),
-                torch.zeros(1, 1, self.hidDim))
+        return (torch.zeros(2 * self.layers, 1, self.hidDim),
+                torch.zeros(2 * self.layers, 1, self.hidDim))
 
     def forward(self, input):
         embeds = self.word_embeddings(input)
@@ -75,31 +93,32 @@ class Tagger(nn.Module):
     def computeLoss(self, input, reference):
         self.zero_grad()
         self.hidden = self.init_hidden()
-        prediction = self(convert2Index(input, word_to_ix))
+        prediction = self(convert2Index(input, w2int))
         return self.lossFunc(prediction,
-                             convert2Index(reference, tag_to_ix))
+                             convert2Index(reference, t2int))
 
 
-model = Tagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+model = Tagger(EMBEDDING_DIM, HIDDEN_DIM, 2, len(w2int), len(t2int))
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 # See what the scores are before training
 # Note that element i,j of the output is the score for tag j for word i.
 # Here we don't need to train, so the code is wrapped in torch.no_grad()
 with torch.no_grad():
-    inputs = convert2Index(training_data[0][0], word_to_ix)
+    inputs = convert2Index(trainData[0][0], w2int)
     prediction = model(inputs)
     print(prediction)
 
-for epoch in range(300):
-    for sentence, tags in training_data:
+for epoch in range(20):
+    print(f"Starting epoch {epoch}")
+    for sentence, tags in tqdm.tqdm(trainData):
         loss = model.computeLoss(sentence, tags)
         loss.backward()
         optimizer.step()
 
 # See what the scores are after training
 with torch.no_grad():
-    inputs = convert2Index(training_data[0][0], word_to_ix)
+    inputs = convert2Index(trainData[0][0], w2int)
     prediction = argmax(model(inputs))
-    prediction = [ix_to_tag[index.item()] for index in prediction]
+    prediction = [int2t[index.item()] for index in prediction]
     print(prediction)
