5353fbb869a504ed133d2c1b9775c22f0d6d2f66
Jetic Gu <jetic@me.com>
2019-03-01 16:39:53 -0800

    Update txt.py

--- natlang/format/txt.py
+++ natlang/format/txt.py
@@ -13,6 +13,6 @@ __version__ = "0.3a"
 
 
 def load(file, linesToLoad=sys.maxsize):
-    with open(os.path.expanduser(file)) as f:
+    with open(os.path.expanduser(file), encoding='utf-8') as f:
         content = [line.lower().strip().split() for line in f][:linesToLoad]
     return content
