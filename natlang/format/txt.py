afe0048d188cca8ad68991817d4dbcb9915f8184
Jetic Gu <jetic@me.com>
2019-03-02 01:01:22 -0800

    Update txt.py

--- natlang/format/txt.py
+++ natlang/format/txt.py
@@ -7,12 +7,13 @@
 #
 #
 from __future__ import absolute_import
+import io
 import os
 import sys
 __version__ = "0.3a"
 
 
 def load(file, linesToLoad=sys.maxsize):
-    with open(os.path.expanduser(file), encoding='utf-8') as f:
+    with io.open(os.path.expanduser(file), encoding='utf-8') as f:
         content = [line.lower().strip().split() for line in f][:linesToLoad]
     return content
