69e99cd126eac6f2c3821440a0c21e81637f3e9d
Ruoyi Wang <endvroy@gmail.com>
2019-03-03 01:45:02 -0800

    ported scratches to bash loader

--- natlang/analysis/bash_code_utils.py
+++ natlang/analysis/bash_code_utils.py
@@ -1,7 +1,5 @@
 import bashlex
 import itertools
-import re
-from operator import itemgetter
 
 
 def proc_line(line):
@@ -94,14 +92,6 @@ def find_words_for_kind(d, indices):
     return words
 
 
-def remap_pos(node, cst_node, line):
-    """remap the pos of a command substitution node `cst_node` to its parent node `node`"""
-    cst_str = line[slice(*cst_node.pos)]
-    remapped_start = node.word.find(cst_str)
-    remapped_end = remapped_start + len(cst_str)
-    return remapped_start, remapped_end
-
-
 def determine_cst_type(cst_str):
     if cst_str.startswith('$(') and cst_str.endswith(')'):
         cst_type = 'dollar'
@@ -110,54 +100,6 @@ def determine_cst_type(cst_str):
     else:
         cst_type = 'unknown'
     return cst_type
-
-
-def determine_pst_type(pst_str):
-    if pst_str.startswith('<(') and pst_str.endswith(')'):
-        pst_type = 'PST_left'
-    elif pst_str.startswith('>(') and pst_str.endswith(')'):
-        pst_type = 'PST_right'
-    else:
-        pst_type = 'unknown'
-    return pst_type
-
-
-def split_segments(node, pos_list):
-    """
-    :param node: Node of type word
-    :param pos_list: [((start:int, end:int), type_name:str, node)] pos of special parts
-    :return: [(type_name: str, {text_segment: str | node})]
-    """
-    pos_list.sort(key=itemgetter(0))
-    segments = []
-    last_end = 0
-    for t in pos_list:
-        pos, type_name, part = t
-        text_segment = node.word[last_end:pos[0]]
-        last_end = pos[1]
-        if text_segment:
-            segments.append(('text', text_segment))
-        # node_text = node.word[slice(*pos)]
-        segments.append((type_name, part))
-    text_segment = node.word[last_end:]
-    if text_segment:
-        segments.append(('text', text_segment))
-    return segments
-
-
-sbtk_engine = re.compile(r'''
-[a-zA-Z]+ | # words
-[0-9]+ |    # numbers
-.   # everything else
-''', re.VERBOSE)
-
-
-def split_subtoken(text):
-    """
-    :param text: str
-    :return: [str]
-    """
-    return sbtk_engine.findall(text)
 
 
 def find_clipped_word_nodes(d):
@@ -171,42 +113,6 @@ def find_clipped_word_nodes(d):
 
 
 CST_ERR = [157, 162, 533, 624, 1681, 4694, 4791, 5499]
-
-CST_KIND = 'commandsubstitution'
-PST_KIND = 'processsubstitution'
-
-
-def process_node(node, line):
-    if node.kind == 'word':
-        pos_list = []
-        for part in node.parts:
-            if part.kind in ('parameter', 'tilde'):
-                pass
-            else:
-                remapped_pos = remap_pos(node, part, line)
-                if part.kind == CST_KIND:
-                    # cst_type = determine_cst_type(part_str)
-                    type_name = 'CST'
-                elif part.kind == PST_KIND:
-                    part_str = node.word[slice(*remapped_pos)]
-                    pst_type = determine_pst_type(part_str)
-                    type_name = pst_type
-                else:
-                    raise RuntimeError('unknown kind {}'.format(part.kinds))
-                assert type_name != 'unknown'
-                pos_list.append((remapped_pos, type_name, part))
-        segments = split_segments(node, pos_list)
-
-        children = []
-        for segment in segments:
-            ty, data = segment
-            if ty == 'text':
-                subtokens = split_subtoken(data)  # todo: wrap each subtoken in a node
-                children.append(subtokens)
-            else:
-                children.append(data)  # todo: transform data recursively
-        return children
-
 
 if __name__ == '__main__':
     train_f = open('/Users/ruoyi/Projects/PycharmProjects/nl2bash/data/bash/train.cm.filtered')
