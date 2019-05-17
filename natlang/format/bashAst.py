d3826de9918f2cde0804cf9cd26012cd9ec8db4a
Ruoyi Wang <endvroy@gmail.com>
2019-05-17 23:30:31 +0800

    updated repr added ast to tokens exporter

--- natlang/format/bashAst.py
+++ natlang/format/bashAst.py
@@ -15,6 +15,147 @@ import json
 
 from natlang.format.astTree import AstNode as BaseNode
 
+# migrated
+import itertools
+
+pattern_argument_types = {
+    'Regex',
+    'File',
+    'Directory',
+    'Path',
+    'Format',
+    'Option',
+    'ReservedWord',
+    'Program',
+    'Bandwidth'
+}
+
+quantity_argument_types = {
+    'Number',
+    '+Number',
+    '-Number',
+    'Quantity',
+    '+Quantity',
+    '-Quantity',
+    'Size',
+    '+Size',
+    '-Size',
+    'Timespan',
+    '+Timespan',
+    '-Timespan',
+    'DateTime',
+    '+DateTime',
+    '-DateTime',
+    'Permission',
+    '+Permission',
+    '-Permission'
+}
+
+argument_types = pattern_argument_types | quantity_argument_types | \
+                 {'Type', 'Unknown'}
+
+
+def get_normalize_tokens(ast, p=None, sketch=False):
+    if ast is None:
+        return []
+    if ast == []:
+        return []
+    if ast.kind.isupper():
+        if sketch:
+            return ['<SKETCH_{}>'.format(p.kind.upper())]
+        # leaf node
+        if ast.kind == 'VAR':
+            # var node
+            # append $
+            return ['$'] + [ast.value[1:]]
+        else:
+            # return the lexeme as-is
+            return [ast.value]
+    elif ast.kind == 'pipeline':
+        if ast.prev is None:
+            prev_tokens = []
+        else:
+            prev_tokens = get_normalize_tokens(ast.prev, ast, sketch)
+            prev_tokens.append('|')
+        return prev_tokens + get_normalize_tokens(ast.last_cmd, ast, sketch)
+    elif ast.kind == 'cmd':
+        assign_list_tokens = get_sstl(ast.assign_list, get_normalize_tokens, sketch)
+        prog_tokens = get_normalize_tokens(ast.prog, ast, sketch)
+        args_tokens = get_sstl(ast.args, get_normalize_tokens, sketch)
+        redir_tokens = get_sstl(ast.redir, get_normalize_tokens, sketch)
+        if assign_list_tokens:
+            assign_list_tokens.append(' ')
+        if args_tokens:
+            args_tokens.insert(0, ' ')
+        if redir_tokens:
+            redir_tokens.insert(0, ' ')
+        tokens = assign_list_tokens + prog_tokens + args_tokens + redir_tokens
+        return tokens
+    elif ast.kind == 'assign':
+        lhs = [ast.lhs]
+        op = [ast.op]
+        rhs = get_normalize_tokens(ast.rhs, ast, sketch)
+        return lhs + op + rhs
+    elif ast.kind == 'assign_rhs':
+        return list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts))
+    elif ast.kind == 'redir':
+        lhs = []
+        if ast.lhs:
+            lhs.append(ast.lhs)
+        rhs = get_normalize_tokens(ast.rhs, ast, sketch)
+        return lhs + [ast.type] + rhs
+    elif ast.kind == 'prog':
+        return list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts))
+    elif ast.kind in ['arg', 'binarylogicop', 'unarylogicop', 'operator']:
+        return list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts))
+    elif ast.kind in argument_types:
+        return list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts))
+    elif ast.kind == 'flag':
+        tokens = [ast.name]
+        if ast.value is not None:
+            tokens.append(' ')
+            tokens.extend(itertools.chain.from_iterable(
+                get_normalize_tokens(x, ast, sketch) for x in ast.value))
+        return tokens
+    elif ast.kind == 'cst':
+        return ['$('] + get_normalize_tokens(ast.pipeline, ast, sketch) + [')']
+    elif ast.kind == 'lpst':
+        return ['<('] + get_normalize_tokens(ast.pipeline, ast, sketch) + [')']
+    elif ast.kind == 'rpst':
+        return ['>('] + get_normalize_tokens(ast.pipeline, ast, sketch) + [')']
+    elif ast.kind == 'arith_subst':
+        return ['$(('] + get_normalize_tokens(ast.pipeline, ast, sketch) + ['))']
+    elif ast.kind == 'param_exp':
+        return ['${'] + list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts)) + ['}']
+    elif ast.kind == 'dquote_str':
+        return ['"'] + list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts)) + ['"']
+    elif ast.kind == 'squote_str':
+        return ["'"] + list(itertools.chain.from_iterable(
+            get_normalize_tokens(x, ast, sketch) for x in ast.parts)) + ["'"]
+    elif ast.kind == 'paren_grp':
+        return ['('] + get_normalize_tokens(ast.pipeline, ast, sketch) + [')']
+    elif ast.kind == 'curly_grp':
+        return ['{', ' '] + get_normalize_tokens(ast.pipeline, ast, sketch) + ['}']
+
+
+def get_sstl(l, f, sketch):
+    """space-separated token list"""
+    token_list = []
+    for node in l:
+        if token_list:
+            token_list.append(' ')
+        token = f(node, sketch=sketch)
+        token_list.extend(token)
+    return token_list
+
+
+# migration END
 
 class AstNode(BaseNode):
     def find_literal_nodes(self):
@@ -77,6 +218,10 @@ def tree2ast(root):
                 continue
             children[kind] = ast_node
             # children.append(ast_node)
+        kind = root.value[0]
+        if kind == 'pipeline':
+            children['last_cmd'] = children['cmd']
+            del children['cmd']
         root_ast_node = BashNode(kind=root.value[0], **children)
         return root.value[0], root_ast_node
 
@@ -219,7 +364,7 @@ class Code:
         return len(self.value)
 
     def __repr__(self):
-        return "<DjangoCode: " + str(self.value) + ">"
+        return "<BashCode: " + str(self.value) + ">"
 
     def __getitem__(self, key):
         return self.value[key]
@@ -231,7 +376,9 @@ class Code:
         return sketch
 
     def export(self):
-        return " ".join(self.value)
+        ast = tree2ast(self.astTree)[1]
+        tokens = get_normalize_tokens(ast)
+        return ''.join(tokens)
 
 
 def load(fileName, linesToLoad=sys.maxsize, verbose=True, option=None,
